704d8120b5353475f961d4cd716c0384c56f341a
Carlos R. Arias <arias@spu.edu>
2019-11-06 10:38:34 -0800

    Now it has functions to read and write from and to streams

--- people.cpp
+++ people.cpp
@@ -7,11 +7,18 @@ using std::cin;
 using std::endl;
 using std::cerr;
 using std::ofstream;
+using std::ifstream;
+using std::ostream;
+using std::istream;
 
 struct Person{
   char name[20];
   int age;
 };
+
+void WritePerson(ostream&, const Person&);
+bool ReadPersonFromKeyboard(istream&, Person&);
+void ReadPerson(istream&, Person&);
 
 int main(int argc, char* argv[]){
   char filename[20];
@@ -26,25 +33,53 @@ int main(int argc, char* argv[]){
 
   Person person;
   do{
-    cout << "Name: ";
-    cin >> person.name;
-    if (strcmp(person.name, "finish") == 0){
+    if (!ReadPersonFromKeyboard(cin, person))
       break;
-    }
-
-    cout << "Age: ";
-    cin >> person.age;
-    while (cin.fail()){
-      cerr << "Wrong input!" << endl;
-      cin.clear();
-      cin.ignore(255, '\n');
-      cout << "Age: ";
-      cin >> person.age;
-    }
-    outputFile << person.name << " " << person.age << endl;
+    WritePerson(outputFile, person);
   }while (true);
 
   outputFile.close();
+
+  cout << endl << endl << endl << "Showing the contents of the file"
+       << endl << endl;
+  cout << "Input file: ";
+  cin >> filename;
+  ifstream inputFile(filename);
+  if (inputFile.fail()){
+    cerr << "Could not open the file for reading " << filename << endl;
+    return 1;
+  }
+  ReadPerson(inputFile, person);
+  while (!inputFile.fail()){
+    WritePerson(cout, person);
+    ReadPerson(inputFile, person);
+  }
+  inputFile.close();
   return 0;
 
+}
+
+void WritePerson(ostream& output, const Person& person){
+  output << person.name << " " << person.age << endl;
+}
+bool ReadPersonFromKeyboard(istream& input, Person& person){
+  cout << "Name: ";
+  input >> person.name;
+  if (strcmp(person.name, "finish") == 0){
+    return false;
+  }
+
+  cout << "Age: ";
+  input >> person.age;
+  while (input.fail()){
+    cerr << "Wrong input!" << endl;
+    input.clear();
+    input.ignore(255, '\n');
+    cout << "Age: ";
+    input >> person.age;
+  }
+  return true;
+}
+void ReadPerson(istream& input, Person& person){
+  input >> person.name >> person.age;
 }
